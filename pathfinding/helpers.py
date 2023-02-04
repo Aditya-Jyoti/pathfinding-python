9c269c3a13e51754f648bd26c65ebb997079a02f
Aditya <rev.toady.py@gmail.com>
2023-02-05 00:39:54 +0530

    Add feature to make multiple end nodes available

--- pathfinding/helpers.py
+++ pathfinding/helpers.py
@@ -3,37 +3,36 @@ from dataclasses import dataclass
 from typing import Optional, List, Tuple
 
 
-TRAVERSED = (255, 0, 0)  # red
-IDLE = (255, 255, 255)  # white
-BLOCKED = (0, 0, 0)  # black
-NEIGHBOUR = (0, 255, 0)  # green
-END = (255, 255, 0)  # yellow
-START = (0, 0, 255)  # blue
+NODE_STATES = {
+    "traversed": (255, 0, 0),  # red
+    "idle": (255, 255, 255),  # white
+    "blocked": (0, 0, 0),  # black
+    "neighbour": (0, 255, 0),  # green
+    "end":(255, 255, 0),  # yellow
+    "start": (0, 0, 255)  # blue
+}
 
 
 def pythagorean_distance(node1: Node, node2: Node):
-    return ((node1.x_pos - node2.x_pos) ** 2 + (node1.y_pos - node2.y_pos) ** 2) * 0.5
+    return ((node1.x_pos - node2.x_pos) ** 2 + (node1.y_pos - node2.y_pos) ** 2) ** 0.5
 
 
-@dataclass
+@dataclass(unsafe_hash= True)
 class Node:
     x_pos: int  # x pos on grid
     y_pos: int  # y pos on grid
-    g_cost: float = 0  # distance from start node
+    g_cost: float = float("inf")  # distance from start node
     h_cost: float = 0  # distance from end node
     f_cost: float = 0  # sum of g_cost and h_cost
     state: str = "idle"  # traversed, idle, blocked, neighbour
-    isStartNode: bool = False  # is it the start node
-    isEndNode: bool = False  # is it the start node
+    weight: Optional[float] = 0 # weight of priority
     parent: Optional[Node] = None  # node originated from
 
     def __eq__(self, comparing_node: Node) -> bool:
         return self.x_pos == comparing_node.x_pos and self.y_pos == comparing_node.y_pos
 
-    def update_node_costs(self, new_h_cost: float, new_g_cost: Optional[float] = None) -> None:
-        if new_g_cost:
-            self.g_cost = new_g_cost
-
+    def update_node_costs(self, new_h_cost: float, new_g_cost: float) -> None:
+        self.g_cost = new_g_cost
         self.h_cost = new_h_cost
         self.f_cost = self.g_cost + self.h_cost
 
@@ -43,3 +42,10 @@ class Node:
             for x, y in [(-1, 0), (0, 1), (1, 0), (0, -1)]
             if 0 <= self.x_pos + x < row_cells and 0 <= self.y_pos + y < col_cells
         ]
+    
+    def renew_node(self) -> None:
+        self.g_cost = float("inf")
+        self.h_cost = 0
+        self.f_cost = 0
+        self.state = "idle"
+        self.parent = None
--- pathfinding/runner.py
+++ pathfinding/runner.py
@@ -11,6 +11,7 @@ class Pathfinder:
         self.cell_size = settings["cell_size"]
         self.row_cells = settings["row_cells"]
         self.col_cells = settings["col_cells"]
+        self.add_wait_time = settings["add_wait_time"]
 
         self.screen = pg.display.set_mode(
             (self.row_cells * self.cell_size, self.col_cells * self.cell_size)
@@ -22,20 +23,19 @@ class Pathfinder:
         ]
 
         self.start_node = None
-        self.end_node = None
-        self.open_nodes = []
-        self.closed_nodes = []
+        self.open_nodes = set()
+        self.closed_nodes = set()
+        self.visual_nodes = []
+        self.end_nodes = []
+        self.path_nodes = []
+        self.backup_nodes = {"start": None, "end": []}
 
     def draw_board(self) -> None:
         for row in self.game_board:
             for node in row:
                 pg.draw.rect(
                     self.screen,
-                    START
-                    if node.isStartNode
-                    else END
-                    if node.isEndNode
-                    else eval(node.state.upper()),
+                    NODE_STATES[node.state],
                     pg.Rect(
                         node.x_pos * self.cell_size,
                         node.y_pos * self.cell_size,
@@ -58,51 +58,98 @@ class Pathfinder:
                 pg.Rect(0, y_idx * self.cell_size, self.row_cells * self.cell_size, 3),
             )
 
-    def solve_path(self, node: Node) -> Node:
-        if node.parent is None:
-            return node
+    def solve_path(self, node: Node) -> bool:
 
-        self.game_board[node.y_pos][node.x_pos].isEndNode = True
+        if node.parent == self.start_node:
+            return True
+
+        self.path_nodes.append(node)
         return self.solve_path(node.parent)
 
     def pathfind(self) -> Optional[bool]:
-        if not isinstance(self.end_node, Node):
-            return
         if not isinstance(self.start_node, Node):
             return
 
-        current_node: Node = sorted(self.open_nodes, key=lambda node: node.f_cost)[0]
-        current_node.state = "traversed"
-        popped = self.open_nodes.pop(self.open_nodes.index(current_node))
-        self.closed_nodes.append(popped)
+        current_node: Node = min(self.open_nodes, key=lambda node: node.f_cost)
+        self.open_nodes.remove(current_node)
 
-        if current_node == self.end_node:
+        current_node.state = "traversed"
+        self.closed_nodes.add(current_node)
+
+        for node in self.end_nodes:
+            if current_node != node:
+                continue
+
+            self.backup_nodes["end"].append(node)
+            self.end_nodes.remove(node)
             self.solve_path(current_node)
-            return True
+
+            for node in self.open_nodes:
+                self.visual_nodes.append(node)
+                node.renew_node()
+
+            if len(self.end_nodes) == 0:
+                for node in self.visual_nodes:
+                    self.game_board[node.y_pos][node.x_pos].state = "traversed"
+
+                for node in self.path_nodes:
+                    node.state = "end"
+                    self.draw_board()
+                    pg.display.update()
+
+                    if self.add_wait_time:
+                        sleep(0.1)
+
+                self.game_board[self.backup_nodes["start"].y_pos][
+                    self.backup_nodes["start"].x_pos
+                ].state = "start"
+
+                for node in self.backup_nodes["end"]:
+                    self.game_board[node.y_pos][node.x_pos].state = "neighbour"
+
+                return True
+
+            for node in self.closed_nodes:
+                if node.state == "blocked":
+                    continue
+
+                self.visual_nodes.append(node)
+                node.renew_node()
+
+            current_node.renew_node()
+            current_node.state = "start"
+            current_node.g_cost = 0
+
+            self.start_node = current_node
+            self.open_nodes = {self.start_node}
+
+            self.closed_nodes = set(
+                filter(lambda node: node.state == "blocked", self.closed_nodes)
+            )
+
+            for node in self.end_nodes:
+                node.weight = pythagorean_distance(self.start_node, node)
+
+            return
 
         for nbr_x, nbr_y in current_node.get_neighbours(self.row_cells, self.col_cells):
             neighbour_node = self.game_board[nbr_y][nbr_x]
 
-            if neighbour_node.state == "blocked" or neighbour_node in self.closed_nodes:
+            if neighbour_node in self.closed_nodes:
                 continue
 
-            new_g_cost = pythagorean_distance(neighbour_node, self.start_node)
-            if (
-                new_g_cost < neighbour_node.g_cost
-                or neighbour_node not in self.open_nodes
-            ):
-                new_h_cost = pythagorean_distance(neighbour_node, self.end_node)
+            new_g_cost = current_node.g_cost + 1
+            if new_g_cost < neighbour_node.g_cost:
+                new_h_cost = pythagorean_distance(
+                    neighbour_node, min(self.end_nodes, key=lambda node: node.weight)
+                )
 
-                if new_g_cost < neighbour_node.g_cost:
-                    neighbour_node.update_node_costs(new_h_cost, new_g_cost)
-                else:
-                    neighbour_node.update_node_costs(new_h_cost)
-
+                neighbour_node.update_node_costs(new_h_cost, new_g_cost)
                 neighbour_node.parent = current_node
                 neighbour_node.state = "neighbour"
 
                 if neighbour_node not in self.open_nodes:
-                    self.open_nodes.append(neighbour_node)
+                    self.open_nodes.add(neighbour_node)
 
             self.game_board[nbr_y][nbr_x] = neighbour_node
 
@@ -115,11 +162,14 @@ class Pathfinder:
                     running = False
 
                 if event.type == pg.KEYDOWN:
+                    self.backup_nodes["start"] = self.start_node
                     if event.key == pg.K_SPACE:
+                        for node in self.end_nodes:
+                            node.weight = pythagorean_distance(self.start_node, node)
+
                         ret = self.pathfind()
                         while not ret:
                             ret = self.pathfind()
-                            sleep(0.1)
                             self.draw_board()
                             pg.display.update()
 
@@ -127,19 +177,29 @@ class Pathfinder:
                     mouse_pos = pg.mouse.get_pos()
                     mouse_pos_x = mouse_pos[0] // self.cell_size
                     mouse_pos_y = mouse_pos[1] // self.cell_size
+                    clicked_node = self.game_board[mouse_pos_y][mouse_pos_x]
 
                     if pg.mouse.get_pressed()[0]:
-                        if not self.start_node:
-                            self.start_node = self.game_board[mouse_pos_y][mouse_pos_x]
-                            self.start_node.isStartNode = True
-                            self.open_nodes.append(self.start_node)
+                        if clicked_node.state == "idle" and not self.start_node:
+                            clicked_node.isStartNode = True
+                            clicked_node.state = "start"
+                            clicked_node.g_cost = 0
+                            self.open_nodes.add(clicked_node)
+                            self.start_node = clicked_node
 
-                        elif not self.end_node:
-                            self.end_node = self.game_board[mouse_pos_y][mouse_pos_x]
-                            self.end_node.isEndNode = True
+                        if self.start_node:
+                            if clicked_node.state == "idle":
+                                clicked_node.state = "end"
+                                clicked_node.isEndingNode = True
+                                self.end_nodes.append(clicked_node)
+
+                            elif clicked_node.state == "end":
+                                self.end_nodes.remove(clicked_node)
+                                clicked_node.isEndingNode = False
+                                clicked_node.state = "idle"
 
                 if event.type == pg.MOUSEMOTION:
-                    if not self.start_node and not self.end_node:
+                    if not self.start_node:
                         continue
 
                     mouse_pos = pg.mouse.get_pos()
@@ -148,23 +208,20 @@ class Pathfinder:
                     pressed = pg.mouse.get_pressed()
                     node = self.game_board[mouse_pos_y][mouse_pos_x]
 
-                    if pressed[0]:
-                        if (
-                            node.state == "idle"
-                            and not node.isStartNode
-                            and not node.isEndNode
-                        ):
+                    if pressed[2]:
+                        if node.state == "idle":
                             node.state = "blocked"
+                            self.closed_nodes.add(node)
 
-                    elif pressed[2]:
-                        if (
-                            node.state == "blocked"
-                            and not node.isStartNode
-                            and not node.isEndNode
-                        ):
-                            node.state = "idle"
+                    # elif pressed[2]:
+                    #     if (
+                    #         node.state == "blocked"
+                    #         and not node.isStartNode
+                    #         and not node.isEndNode
+                    #     ):
+                    #         node.state = "idle"
+                    #         self.closed_nodes.remove(node)
 
             self.draw_board()
             self.clock.tick(60)
             pg.display.update()
-
